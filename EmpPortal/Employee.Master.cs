e823a25f6fd0eb681943d0c270e8e245f9691641
ashishkhadye33 <ashishkhadye33@gmail.com>
2024-02-26 12:41:20 +0530

    aspnet project day 1

---
+++ EmpPortal/Employee.Master.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmpPortal
+{
+    public partial class Employee : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["EmployeeId"] != null)
+            {
+                lilogout.Visible = true;
+                liregister.Visible = false;
+                lilogin.Visible = false;
+            }
+            else
+            {
+                lilogout.Visible = false;
+                liregister.Visible = true;
+                lilogin.Visible = true;
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Response.Redirect("Index.aspx");
+        }
+    }
+}
