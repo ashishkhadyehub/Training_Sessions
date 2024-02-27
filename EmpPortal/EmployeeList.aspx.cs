de868ee943f48e2d7e78a886bddd4535d2de7c0e
ashishkhadye33 <ashishkhadye33@gmail.com>
2024-02-27 12:17:25 +0530

    aspnet project day 2 update

---
+++ EmpPortal/EmployeeList.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmpPortal
+{
+    public partial class EmployeeList : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Admin"]!= null)
+            {
+                gvdata.DataSource = getlist();
+                gvdata.DataBind();
+            }
+            else
+            {
+                Response.Redirect("AdminLogin.aspx");
+            }
+        }
+
+        protected DataSet getlist()
+        {
+            SqlCommand cmd = new SqlCommand("select * from EmpRegister order by Srno DESC",con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            return  ds;
+        }
+    }
+}
